c998a5cf7a29694d06f32102ef73473c0fcc7d1b
Bhavya Tyagi <55448429+bhavya2654@users.noreply.github.com>
2020-02-23 03:18:41 +0530

    Create conf.py

---
+++ conf.py
@@ -0,0 +1,19 @@
+"""//Everything here is encrypted , please enter your own Credentials"""
+"""For SMS"""
+SSID = 'ACd4d24bf6770284eXXXXXX4f347af237b' 
+AUTH_TOKEN = 'afd8a3d47XXXXXX7bcd8459843aa1d43' 
+FROM_NUMBER = '+13342XXX511'
+TO_NUMBER = '+918527XXX287'
+API_KEY = '748b4302-0c4d-43ce-aaee-3c5XXXe4acd7'
+DEVICE_ID = 'BOLT29XXX4'
+
+"""FOR EMAIL"""
+MAILGUN_API_KEY = 'c2abe147695f14eac73cd1XXXd82e890-a9919d1f-02894bb7' 
+SANDBOX_URL= 'sandbox82a4fab99cf74b17XXXf46c824c70d78.mailgun.org' 
+SENDER_EMAIL = 'test@sandbox82a4XXX99cf74b17879f46c824c70d78.mailgun.org'
+RECIPIENT_EMAIL = 'bhavyatyagi16@gmail.com'
+API_KEY = '748b4302-0c4d-43ce-aaee-3c5XXXe4acd7'
+DEVICE_ID = 'BOLT2XXX34'
+
+FRAME_SIZE = 10
+MUL_FACTOR = 6
